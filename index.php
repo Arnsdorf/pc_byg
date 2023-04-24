a176cb955997203b7005d6f081aaaed3d0f35bde
Arnsdorf <38246817+Arnsdorf@users.noreply.github.com>
2023-04-24 14:22:58 +0200

    1.2 - Start på main section

---
+++ index.php
@@ -0,0 +1,72 @@
+
+
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Lille Test</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<!-- pc-byg -->
+<section>
+    <div class="container">
+        <div class="row d-flex justify-content-center mt-5">
+            <div class="pt-5">
+                <h2 class="display-6 text-center text-white fw-normal">Sammensæt din egen gamer-computer😍</h2>
+                <p class="pt-3 pb-5 fs-5 text-white text-center p-max mx-auto">
+                    Byg din egen drømmecomputer med vores omfattende udvalg af hardwarekomponenter og skræddersy din oplevelse til dine behov og ønsker.
+                </p>
+            </div>
+            <div class="pb-3">
+                <h2 class="rounded-top text-white mt-3 pb-3">Vælg 2 gamer-spil.</h2>
+
+                <div class="container">
+
+                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 d-flex justify-content-center">
+
+                        <div class="card image shadow p-0 m-2 border-0 rounded-5">
+                            <img src="images/fortnite.png" alt="fortnite" class="card-img">
+                        </div>
+
+                        <div class="card image shadow p-0 m-2 border-0 rounded-5">
+                            <img src="images/gta.png" alt="gta" class="card-img">
+                        </div>
+
+                        <div class="card image shadow p-0 m-2 border-0 rounded-5">
+                            <img src="images/hoqwarts.png" alt="hogwarts" class="card-img">
+                        </div>
+
+                        <div class="card image shadow p-0 m-2 border-0 rounded-5">
+                            <img src="images/csgo.png" alt="csgo" class="card-img">
+                        </div>
+
+                        <div class="card image shadow p-0 m-2 border-0 rounded-5">
+                            <img src="images/minecraft.png" alt="minecraft" class="card-img">
+                        </div>
+
+                        <div class="card image shadow p-0 m-2 border-0 rounded-5">
+                            <img src="images/warzone.png" alt="warzone" class="card-img">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
