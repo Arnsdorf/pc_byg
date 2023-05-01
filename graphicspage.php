bf12e32643929132ec7b7226c27bbf3d8225b963
Arnsdorf <38246817+Arnsdorf@users.noreply.github.com>
2023-05-01 15:59:57 +0200

    1.8

--- graphicspage.php
+++ graphicspage.php
@@ -19,9 +19,33 @@
 </head>
 <body>
 
-<div class="d-flex justify-content-center align-items-center">
-    <h1 class="text-white">HEJ</h1>
-</div>
+<!-- pc-byg -->
+<section>
+    <div class="pt-5">
+        <h2 class="display-6 text-center text-white fw-bold">Vælg dine top 2 <span>spil.</span></h2>
+        <p class="pt-3 pb-5 fs-5 text-white text-center p-max mx-auto">
+            Vælg de <span>2 spil</span> du interessere dig for mest, eller de spil du godt kunne tænke dig at spille!
+        </p>
+    </div>
+    <div class="container-fluid shadow" style="background-color: #001D31">
+        <div class="row d-flex justify-content-center mt-sm-5 mb-sm-5">
+            <div class="pb-3 pt-3">
+                <div class="container">
+
+                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 d-flex justify-content-center">
+                        <div class="shadow p-0 m-2 border-0 rounded-5">
+                            <img src="images/Battlefield%20V%20Screenshot%202023.04.27%20-%2010.45.31.55.png" alt="graphic_game" class="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center mt-sm-5 flex-sm-row flex-column">
+        <a class="m-2 p-3 bg-black text-white text-decoration-none shadow" href="#">< Gå tilbage</a>
+        <a class="m-2 p-3 bg-black text-white text-decoration-none shadow" id="submit_btn">Gå videre ></a>
+    </div>
+</section>
 
 </body>
 </html>
