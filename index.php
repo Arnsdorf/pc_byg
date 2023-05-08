afbbf4931ddf6984c106f2ec5bd1d225d8fc2dbb
Christian <Chrtobias4780@gmail.com>
2023-05-08 14:55:54 +0200

    Chr 1.0.0

--- index.php
+++ index.php
@@ -20,7 +20,9 @@
 	<link rel="icon" href="images/buildmate_favicon_green.svg">
 
 	<!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -29,8 +31,18 @@
 <!-- i <body> har man alt indhold på siden som brugeren kan se -->
 <body>
 
+<div class="image_box position-relative">
+    <img class="image_front" src="images/cichlid_visuals-desk-setup.jpg" alt="frontpage">
 
+    <div class="frontpage-text position-relative justify-content-center d-flex ">
+        <h1 class="front-font display-6">Spil efter dine:
+            <span id="typewriter-text"></span></h1>
+    </div>
 
+    <div class="paragraph position-relative justify-content-center d-flex ">
+        <h5 class="paragraph-font text-center">Byg en computer der passer dine
+            behov med BuildMate</h5>
+    </div>
 <script src="js/forside.js"></script>
 </body>
 </html>
