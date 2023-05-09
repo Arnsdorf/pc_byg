f4b9084d84f76256b0390170d4eaac3e4607dfce
Chrto <chrtobias4780@gmail.com>
2023-05-09 14:04:35 +0200

    Chr 1.0.1

--- index.php
+++ index.php
@@ -31,18 +31,74 @@
 <!-- i <body> har man alt indhold på siden som brugeren kan se -->
 <body>
 
-<div class="image_box position-relative">
-    <img class="image_front" src="images/cichlid_visuals-desk-setup.jpg" alt="frontpage">
+<!---------- Frontpage ------------->
+<div class="image_box position-relative shadow">
+    <img class="image_front" src="images/gaming-room-designs-blurr.jpg" alt="frontpage">
 
+    <!-- Front-text -->
     <div class="frontpage-text position-relative justify-content-center d-flex ">
         <h1 class="front-font display-6">Spil efter dine:
             <span id="typewriter-text"></span></h1>
     </div>
 
     <div class="paragraph position-relative justify-content-center d-flex ">
-        <h5 class="paragraph-font text-center">Byg en computer der passer dine
-            behov med BuildMate</h5>
+        <h5 class="paragraph-font text-center">Byg en computer der passer dine behov <br> med BuildMate</h5>
     </div>
+    <!-- Front-text-end -->
+
+    <!-- Social-icon-front -->
+    <div class="icon_box icons position-relative justify-content-center justify-content-md-start d-flex">
+       <a class="text-decoration-none text-white" href="#" target="_blank"> <i class="bi bi-facebook ps-md-3 icon_front"></i></a>
+        <a class="text-decoration-none text-white" href="#" target="_blank">  <i class="bi bi-instagram ps-md-3 px-3 px-md-0 icon_front"></i></a>
+            <a class="text-decoration-none text-white" href="#" target="_blank" >  <i class="bi bi-linkedin ps-md-3 icon_front"></i></a>
+    </div>
+    <!-- Social-icon-front-end -->
+
+    <!-- Forside knap -->
+    <div class="button_box d-flex position-relative justify-content-center">
+    <div class="button rounded-2 justify-content-center"><span class="justify-content-center d-flex pc-font p-2 ">Byg en pc</span></div>
+    </div>
+    <!-- Forside knap-end -->
+
+    <!-- Scroll animation -->
+    <div class="d-flex justify-content-center scroll_icon position-relative ">
+        <div class='scrolldown'>
+            <div class="chevrons ">
+                <div class='chevrondown'></div>
+                <div class='chevrondown'></div>
+            </div>
+        </div>
+    </div>
+    <!-- Scroll animation-end -->
+</div>
+
+<!---------- Frontpage-end ------------->
+
+<div class="john ">
+    <div class="row p-0 m-0">
+
+        <div class="john1 position-absolute">
+
+            <div class="card_box ">
+                <div class="row gap-5 p-0 m-0 justify-content-center">
+
+                <div class="card_front shadow col-10 col-md-5 col-lg-3 mt-3"></div>
+                <div class="card_front shadow col-10 col-md-5 col-lg-3 mt-3 mb-3"></div>
+                <div class="card_front shadow col-10 col-md-5 col-lg-3 mb-3 mt-md-3"></div>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
+
 <script src="js/forside.js"></script>
 </body>
 </html>
