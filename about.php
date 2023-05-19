b9871389f948e7f0ccf07f32a4b206c5dab9c5d5
Chrto <chrtobias4780@gmail.com>
2023-05-19 17:45:13 +0200

    Chr 1.0.7 about-us-page

--- about.php
+++ about.php
@@ -39,34 +39,50 @@
 <div class="container-fluid about-us ">
     <div class="row justify-content-center d-flex ">
 
-        <div class="pt-5 text-center"><h1>Hvem er <span>BuildMate</span></h1></div>
-        <div class="pt-2 text-center"><h5>Hvem er vi?</h5></div>
+        <div class="pt-5 text-center"><h1 class="about_header">Hvem er <span>BuildMate</span></h1></div>
+        <div class="pt-3 pb-2 text-center"><h5 class="about_subheader">Hvem er BuildMate?</h5></div>
 
-        <div class="about_card_omega shadow d-none d-md-block mt-4 ">
-            <div class="img-about"> <img src="ABOUT-IMG/billed-animation.gif"> </div>
+        <div class="shadow d-none d-md-block mt-4 p-0 m-0">
+            <div class="about_card_omega"> <img class="img-about" src="images/SquadAnimationgif.gif"> </div>
         </div>
 
-        <div class="about_card shadow col-10 d-md-none mt-5 justify-content-center">
-            <div class="img-about"><img src="ABOUT-IMG/IMG_7925.jpg"> </div>
+        <div class="d-none d-md-block">
+            <div class="justify-content-center justify-content-evenly d-flex pt-5 pb-5">
+                <img src="images/object-group-solid.png">
+                <img class="" src="images/code-solid.png">
+                <img src="images/computer-solid.png">
+            </div>
+            <div class="justify-content-center justify-content-evenly d-flex text-center ">
+                <h2 class=" intro-text">Designer</h2>
+                <h2 class=" intro-text">Koder</h2>
+                <h2 class=" intro-text">Computer-bygning</h2>
+            </div>
         </div>
 
-        <div class=""></div>
-
-        <div class="about_card shadow col-10 d-md-none  mt-5 mb-5 justify-content-center">
-            <div class="img-about"><img src="ABOUT-IMG/IMG_7935.jpg"> </div>
+        <div class="p-0 m-0 shadow col-10 d-md-none mt-5 justify-content-center">
+            <div class="about_card "><img class="img-about" src="ABOUT-IMG/IMG_7925.jpg"> </div>
         </div>
 
-        <div class=""></div>
+        <div class="justify-content-center d-flex pb-3 pt-5 d-md-none"><img src="images/object-group-solid.png"></div>
 
-        <div class="about_card shadow col-10 d-md-none mb-5 justify-content-center">
-            <div class="img-about"><img class="" src="ABOUT-IMG/IMG_7977.jpg"> </div>
+        <div class="p-0 m-0 shadow col-10 d-md-none  mt-5 mb-5 justify-content-center">
+            <div class="about_card "><img class="img-about" src="ABOUT-IMG/IMG_7935.jpg"> </div>
         </div>
 
-        <div class=""></div>
+        <div class="justify-content-center d-flex pb-5 d-md-none"><img src="images/code-solid.png"></div>
+
+        <div class="p-0 m-0 shadow col-10 d-md-none mb-5 justify-content-center">
+            <div class="about_card"><img class="img-about" src="ABOUT-IMG/IMG_7977.jpg"> </div>
+        </div>
+
+        <div class="justify-content-center d-flex pb-5 d-md-none"><img src="images/computer-solid.png"></div>
 
     </div>
 </div>
 
+<div class="filler">
+
+</div>
 
 
 
