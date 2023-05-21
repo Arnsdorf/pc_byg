2c442f9c4715108acdc91c5798846bdc872d6a2b
Christian <Chrtobias4780@gmail.com>
2023-05-21 21:31:12 +0200

    Chr .....

--- about.php
+++ about.php
@@ -40,24 +40,27 @@
     <div class="row justify-content-center d-flex ">
 
         <div class="pt-5 text-center"><h1 class="about_header">Hvem er <span>BuildMate</span></h1></div>
-        <div class="pt-3 pb-2 text-center"><h5 class="about_subheader">Hvem er BuildMate?</h5></div>
+        <div class="pt-3 pb-2 text-center mb-lg-4"><h5 class="about_subheader">Hvem er BuildMate?</h5></div>
 
-        <div class="shadow d-none d-md-block mt-4 p-0 m-0">
+        <div class="shadow d-none d-md-block mt-5 p-0 m-0">
             <div class="about_card_omega"> <img class="img-about" src="images/SquadAnimationgif.gif"> </div>
         </div>
 
-        <div class="d-none d-md-block">
+        <div class="d-none d-md-block ">
             <div class="justify-content-center justify-content-evenly d-flex pt-5 pb-5">
-                <img src="images/object-group-solid.png">
+                <img class="" src="images/object-group-solid.png">
                 <img class="" src="images/code-solid.png">
-                <img src="images/computer-solid.png">
-            </div>
-            <div class="justify-content-center justify-content-evenly d-flex text-center ">
-                <h2 class=" intro-text">Designer</h2>
-                <h2 class=" intro-text">Koder</h2>
-                <h2 class=" intro-text">Computer-bygning</h2>
+                <img class="" src="images/computer-solid.png">
             </div>
         </div>
+
+        <div class="justify-content-center d-flex justify-content-evenly ps-5 d-none">
+            <h2 class=" intro-text">Designer</h2>
+            <h2 class=" intro-text">Koder</h2>
+            <h2 class=" intro-text">Computer-bygning</h2>
+        </div>
+
+
 
         <div class="p-0 m-0 shadow col-10 d-md-none mt-5 justify-content-center">
             <div class="about_card "><img class="img-about" src="ABOUT-IMG/IMG_7925.jpg"> </div>
